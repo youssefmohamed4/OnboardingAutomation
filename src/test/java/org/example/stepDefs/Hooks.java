68c93c14ee0617db185bd0eae586db4654c66392
youssefmohamed4 <74436492+youssefmohamed4@users.noreply.github.com>
2023-07-02 23:30:46 +0300

    Changed setup of the web driver

--- src/test/java/org/example/stepDefs/Hooks.java
+++ src/test/java/org/example/stepDefs/Hooks.java
@@ -4,16 +4,16 @@ import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 
 import java.util.concurrent.TimeUnit;
 
 public class Hooks {
-    public static WebDriver driver = new ChromeDriver();
+    public static WebDriver driver = new FirefoxDriver();
     @Before
     public static void openBrowser() {
-        WebDriverManager.chromedriver().setup();
+        WebDriverManager.firefoxdriver().setup();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(8, TimeUnit.SECONDS);
         driver.get("https://portal-demo.finflx.com/register/");
